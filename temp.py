c7fa8ee060aff4af6a5f6511043d5f866542a676
ShellRedia <34744491+ShellRedia@users.noreply.github.com>
2025-11-10 15:49:10 +0800

    Delete temp.py

--- temp.py
+++
@@ -1,9 +0,0 @@
-import os
-
-dataset_dir = "assets/datasets/OCTA-25K/3M"
-
-for sub_dir in "gradable", "outstanding", "ungradable":
-    image_dir = "{}/{}".format(dataset_dir, sub_dir)
-    for image_file in os.listdir(image_dir):
-        sample_id = int(image_file[:-4]) + 1
-        os.rename("{}/{}".format(image_dir, image_file), "{}/{:0>3}.png".format(image_dir, sample_id))
